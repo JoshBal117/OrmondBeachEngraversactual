4990bc59df4d51f881bda9cff5b78c9551e3cf08
Joshua Balao <JoshuaCBalao@gmail.com>
2024-01-06 16:55:36 -0500

    Update app.js
    reset App.js for better implementation

--- app.js
+++ app.js
@@ -1,24 +1,6 @@
 // The is will be the JS for the backbone of the website
 //unless another option or a better version is created//
 
-$(document).ready(function() {
-
-
-    const imageUrls = [
-        'image1.jpg',
-        'image2.jpg',
-        'image3.jpg',
-    ];
-    
-    function showImage(index) {
-        const carousel = docyment.getElementByid('image-carousel');
-        const offset = -index * 100 + '%';
-    }
-
-}
-
-)
-
 
 
 
