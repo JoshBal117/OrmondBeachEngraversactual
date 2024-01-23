a417e125af2369939c54abc51d0ea657bc643454
Joshua Balao <JoshuaCBalao@gmail.com>
2024-01-22 20:34:22 -0500

    edited the navbar
    created the navbar for the website and created the links for the pages on the website.

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 // The is will be the JS for the backbone of the website
 //unless another option or a better version is created//
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
 
-
-
-
-function toggleNavbar() {
-    const nav = document.querySelector('nav');
-    nav.classList.toggle('show');
-}
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
