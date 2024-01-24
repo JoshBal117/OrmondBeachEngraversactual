08a1a29098371ab9845025b9b11cb11c0adb1d66
Joshua Balao <JoshuaCBalao@gmail.com>
2024-01-23 20:54:47 -0500

    adding lists to index
    Adding lines for better visibility and readability for materials

--- app.js
+++ app.js
@@ -6,3 +6,19 @@ const navLink = document.querySelector('.nav__link');
 hamburger.addEventListener('click', () => {
   navLink.classList.toggle('hide');
 });
+
+
+
+
+const pre = document.getElementById('pre-btn')
+const nxt = document.getElementById('nxt-btn')
+const list = document.getElementById('item-list')
+const itemWidth = 150
+const padding = 10
+
+pre.addEventListener('click', () => {
+    list.scrollLeft -= (itemWidth + padding)
+})
+nxt.addEventListener('click',()=>{
+    list.scrollLeft += (itemWidth + padding)
+})
