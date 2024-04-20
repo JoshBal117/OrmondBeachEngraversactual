ecc5ad43e7f043662512c176d808060fe306832a
Joshua Balao <JoshuaCBalao@gmail.com>
2024-04-19 20:25:00 -0400

    added new features and waiting for full implementation

--- app.js
+++ app.js
@@ -36,3 +36,54 @@ function sendMessageToEmail() {
   // Open default email client with pre-filled email
   window.location.href = mailtoLink;
 }
+
+
+
+const form = document.querySelector('form');
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+  const bodyMessage = 'Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Message: $(mess.value}<br>';
+
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "username",
+    Password : "password",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : subject.value,
+    Body : bodyMessage
+}).then(
+  message => {
+    if (message == 'OK') {
+      Swal.fire({
+        title:"Success!",
+        text: "Message sent successfully",
+        icon: 'success'
+      }); 
+    }
+  }
+);
+}
+
+function checkInputs() {
+  const items = document.querySelectorAll('.items');
+
+  for (const item of items) {
+      if (item.value == "") {
+          item.classList.add("error");
+          item.parentElement.classList.add("error");
+      }
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+
+  //sendEmail();
+}); 
